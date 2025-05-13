ef5e71c3bf43c26d4d685f7c00bc3fc790ff8208
ryusu12 <rsy7567@gmail.com>
2025-05-13 14:20:22 +0900

    :memo: docs: README
    필수과제 ERD 수정

--- README.md
+++ README.md
@@ -4,11 +4,14 @@
 ## 📌 주요 기능
 - 일정 CRUD (생성, 조회, 수정, 삭제)
 
+Lv 1. 일정 생성 및 조회<br>
+Lv 2. 일정 수정 및 삭제
+
 ## 📑 API 명세서
 
 [API 명세서](https://documenter.getpostman.com/view/44733463/2sB2jAaTZM)
 
 ## 🗂️ ERD
 
-![imgERD.png](readme/imgERD.png)
-![sql.png](readme/sql.png)
+![imgERD.png](readme/essentialERD.png)
+![sql.png](readme/essentialSql.png)
--- schedule.sql
+++ schedule.sql
@@ -7,19 +7,3 @@ CREATE TABLE `Schedule`
     `create_date` DATE         NOT NULL COMMENT '작성일',
     `update_date` DATE         NOT NULL COMMENT '수정일'
 );
-
-CREATE TABLE `User`
-(
-    `user_id`     BIGINT AUTO_INCREMENT PRIMARY KEY COMMENT '작성자_ID',
-    `name`        VARCHAR(100) NOT NULL COMMENT '이름',
-    `email`       VARCHAR(100) NOT NULL COMMENT '이메일',
-    `create_date` DATE         NOT NULL COMMENT '작성일',
-    `update_date` DATE         NOT NULL COMMENT '수정일'
-);
-
-ALTER TABLE `Schedule`
-    ADD COLUMN `user_id` BIGINT NOT NULL COMMENT '작성자_ID';
-
-ALTER TABLE `Schedule`
-    ADD CONSTRAINT `FK_User_TO_Schedule_1`
-        FOREIGN KEY (`user_id`) REFERENCES `User` (`user_id`);
